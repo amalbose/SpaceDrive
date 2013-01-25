4ac6a160a97ce1f76a4ddb298c2249dd7325af86
axatrikx <axatrikx@gmail.com>
2013-01-25 18:36:37 +0530

    Added level manager class

---
+++ axagame/game/levelman.cpp
@@ -0,0 +1,37 @@
+/*************************************************************************************
+ *   Created on: Jan 25, 2013
+ *
+ *	SpaceDrive - http://axatrikx.com
+ *	Copyright (C) 2013  Amal Bose
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **************************************************************************************/
+
+#include "levelman.h"
+
+LevelManager::LevelManager() {
+	// TODO Auto-generated constructor stub
+}
+
+void LevelManager::parseLevelXML(const char* xmlFile) {
+	std::string xmlString = Utils::get_file_contents(xmlFile);
+	rapidxml::xml_document<> doc;
+	char* cstr = new char[xmlString.size() + 1];  // Create char buffer to store string copy
+	strcpy(cstr, xmlString.c_str());  // Copy string into char buffer
+	doc.parse<0>(cstr);
+	delete[] cstr;
+	rapidxml::xml_node<>* cur_node = doc.first_node("axagame");
+	cur_node = cur_node->first_node("states")->first_node("state")->first_node("levels")->first_node("level");
+	std::cout << cur_node->first_node("level-name")->value();
+}
---
+++ axagame/game/levelman.h
@@ -0,0 +1,38 @@
+/*************************************************************************************
+ *   Created on: Jan 25, 2013
+ *
+ *	SpaceDrive - http://axatrikx.com
+ *	Copyright (C) 2013  Amal Bose
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **************************************************************************************/
+
+#ifndef LEVELMAN_H_
+#define LEVELMAN_H_
+
+#include <cstring>
+#include <string>
+#include <sstream>
+#include "../../lib/rapidxml/rapidxml.hpp"
+#include "../utils/utils.h"
+
+class LevelManager {
+public:
+	LevelManager();
+
+	static void parseLevelXML(const char* xmlFile);
+
+};
+
+#endif /* LEVELMAN_H_ */

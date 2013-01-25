fb61f6b67bab77ff228e8dc995c746bdf1fecee3
axatrikx <axatrikx@gmail.com>
2013-01-25 15:13:38 +0530

    Modified onEvent to handle state event handles

--- axagame/engine/event.cpp
+++ axagame/engine/event.cpp
@@ -40,9 +40,12 @@ bool Event::OnEvent(const SEvent &event) {
 			if (event.KeyInput.Key == KEY_KEY_X) {
 				Game::Instance().setDone(true);
 			}
+			Game::Instance().getCurrentState()->onKeyPress(event.KeyInput.Key);
 		} else if (!event.KeyInput.PressedDown) {
-
+			Game::Instance().getCurrentState()->onKeyLift(event.KeyInput.Key);
 		}
+
+		setKeyState(event.KeyInput.Key, event.KeyInput.PressedDown);
 		break;
 	case EET_MOUSE_INPUT_EVENT:
 
@@ -50,11 +53,25 @@ bool Event::OnEvent(const SEvent &event) {
 		case EMIE_LMOUSE_PRESSED_DOWN:
 		case EMIE_RMOUSE_PRESSED_DOWN:
 		case EMIE_MMOUSE_PRESSED_DOWN:
+			setMouseState(event.MouseInput.Event, true);
+			return Game::Instance().getCurrentState()->onMousePress(event.MouseInput.Event, event.MouseInput.X,
+					event.MouseInput.Y);
+
 		case EMIE_LMOUSE_LEFT_UP:
 		case EMIE_RMOUSE_LEFT_UP:
 		case EMIE_MMOUSE_LEFT_UP:
+			setMouseState(event.MouseInput.Event - MOUSE_COUNT, false);
+			Game::Instance().getCurrentState()->onMouseLift(event.MouseInput.Event - MOUSE_COUNT, event.MouseInput.X,
+					event.MouseInput.Y);
+			break;
 		case EMIE_MOUSE_MOVED:
+			mouseX = event.MouseInput.X;
+			mouseY = event.MouseInput.Y;
+			Game::Instance().getCurrentState()->onMouseMotion(mouseX, mouseY);
+			break;
 		case EMIE_MOUSE_WHEEL:
+			Game::Instance().getCurrentState()->onMouseWheel(event.MouseInput.Wheel);
+			break;
 		default:
 			break;
 		}

7bd8f137a30713e474e9c2943b28773cb9f418e0
axatrikx <axatrikx@gmail.com>
2013-02-08 22:40:47 +0530

    Added documentation

--- axagame/engine/constants.h
+++ axagame/engine/constants.h
@@ -21,14 +21,40 @@
 #ifndef CONSTANTS_H_
 #define CONSTANTS_H_
 
+/**
+ * Game version number.
+ */
 #define GAME_VERSION  "v0.1.1"
+
+/**
+ * The name of the game.
+ */
 #define GAME_NAME  L"SpaceDrive"
+
+/**
+ * The log file name.
+ */
 #define LOG_NAME  "spacedrive.log"
+
+/**
+ * The file path for configuration property file which holds the Irrlicht device settings.
+ */
 #define CONFIG_FILE "data/config.prop"
 
+/**
+ * File path for states xml file for holding level resource details.
+ */
 #define STATE_DATA "data/states-data.xml"
+
+/**
+ * File Path for key configuration file.
+ */
 #define KEY_CONFIG "data/keys.config"
+
 //states
+/**
+ * The PLAYSTATE refering to game playing state.
+ */
 #define STATE_PLAY "PLAYSTATE"
 
 #endif /* CONSTANTS_H_ */

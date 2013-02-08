13ec2191d0f7c66ca5ea62f44d0ec6f9bd68e519
axatrikx <axatrikx@gmail.com>
2013-02-08 22:36:21 +0530

    Added javadoc for config class

--- axagame/engine/config.h
+++ axagame/engine/config.h
@@ -27,34 +27,102 @@
 #include "../utils/properties.h"
 #include <string>
 
+/**
+ * Manages the settings for Irrlicht Driver.
+ */
 class ConfigClass {
 public:
+	/**
+	 * Constructor to set configuration file.
+	 */
 	ConfigClass();
+	/**
+	 * Initializes configuration.
+	 * @return 1 or 0 based on successful initialization.
+	 */
 	int initConfig();
+
+	/**
+	 * Closes configuration.
+	 */
 	void closeConfig();
+
+	/**
+	 * Resets configuration.
+	 * @return 0 or 1 based on successful reset.
+	 */
 	int resetConfig();
 
+	/**
+	 * Reads the configuration file.
+	 * @return 0 or 1 based on successful execution.
+	 */
 	int readConfig();
+
+	/**
+	 * Writes the current configuration to file.
+	 * @return 0 or 1 based on successful execution.
+	 */
 	int writeConfig();
 
+	/**
+	 * Returns the type of Driver (E_DRIVER_TYPE)
+	 * @return the type of Driver (E_DRIVER_TYPE)
+	 */
 	E_DRIVER_TYPE getDriverType() const { return driverType; }
 
+	/**
+	 * Returns anisotropicfiltering value.
+	 * @return
+	 */
 	int getAnisotropicFiltering() const {		return anisotropicFiltering;	}
 
+	/**
+	 * Returns AntiAliasing value.
+	 * @return AntiAliasing value.
+	 */
 	int getAntiAliasing() const {		return antiAliasing;	}
 
+	/**
+	 * Gets bit rate.
+	 * @return bit rate
+	 */
 	int getBits() const {		return bits;	}
 
+	/**
+	 * Return the configuration file
+	 * @return configuration file.
+	 */
 	char* getConfigFile() const {		return configFile;	}
 
+	/**
+	 * Whether fullscreen or not.
+	 * @return TRUE if set to fullscreen or FALSE otherwise.
+	 */
 	bool getFullscreen() const {		return fullscreen;	}
 
+	/**
+	 * Gets the screen height.
+	 * @return screen height
+	 */
 	int getScreenHeight() const {		return screenHeight;	}
 
+	/**
+	 * Gets the screen width.
+	 * @return screen width
+	 */
 	int getScreenWidth() const {		return screenWidth;}
 
+	/**
+	 * Whether stencil set or not.
+	 * @return
+	 */
 	bool getStencil() const {		return stencil;	}
 
+	/**
+	 * Whether vsync is set or not.
+	 * @return
+	 */
 	bool getVsync() const {		return vsync;	}
 
 private:
@@ -63,10 +131,25 @@ private:
 	E_DRIVER_TYPE driverType;
 	bool fullscreen, shadows, trilinearFiltering, shaders, stencil, vsync;
 	char * configFile;
+
+	/**
+	 * Gets the driver type based on string input
+	 * @param type string value for driver type
+	 * @return
+	 */
 	int getDriverType(std::string type);
+
+	/**
+	 * Gets boolean value from string value.
+	 * @param boolVal
+	 * @return
+	 */
 	bool getBool(std::string boolVal);
 };
 
+/**
+ * Singleton class for ConfigClass.
+ */
 typedef Singleton<ConfigClass> Config;
 
 #endif /* CONFIG_H_ */
